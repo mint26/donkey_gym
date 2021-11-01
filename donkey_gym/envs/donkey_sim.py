60613fa2d86a6eeaa2f6f4c05222056ec0c20831
Hui Min <tan.huimin26@gmail.com>
2021-11-01 19:24:20 +0800

    Set verbose to true

--- donkey_gym/envs/donkey_sim.py
+++ donkey_gym/envs/donkey_sim.py
@@ -33,7 +33,7 @@ class DonkeyUnitySimContoller():
     def __init__(self, level, time_step=0.05, port=9090):
         self.level = level
         self.time_step = time_step
-        self.verbose = False
+        self.verbose = True
         self.wait_time_for_obs = 0.1
 
         # sensor size - height, width, depth
